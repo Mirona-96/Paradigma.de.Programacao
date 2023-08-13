3089d85bf1b48882d94e298f335b67c7dcc65527
HomePC <Eltonmirona10@gmail.com>
2023-08-13 18:45:15 +0200

    Exercicios com Operadores - Paradigma de programacao

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 1 + 5*3/3;
+        System.out.println("1 + 5*3/3 = " + x);
+
+        int j = (3 + 5*3)/3;
+        System.out.println("(3 + 5*3)/3 = " + j);
+
+        int k = 4%3;
+        System.out.println("4%3 = "+ k);
+
+        float f = 7/2;
+        System.out.println("7/2 = " + f);
+
+        int r = 7/2;
+        System.out.println("7/2 = " + r);
+
+        double d = (double)7/2;
+        System.out.println("(double)7/2 = " + d);
+
+        double d1 = x + 7*r - 9;
+        System.out.println("x + 7*r - 9 = " + d1);
+
+        byte b = 8;
+
+        double d2 = (10/3) + (15/3);
+        System.out.println("(10/3) + (15/3) = " + d2);
+    }
+}
